24c0a7bc1c250399392c2949814fb1e37dc937b5
Aetias <aetias@outlook.com>
2025-06-15 16:54:00 +0200

    Share `AmbiguousRelocation` type with C++ context

--- dsd/src/lib.rs
+++ dsd/src/lib.rs
@@ -1,17 +1,39 @@
 pub mod relocs;
-
 use relocs::*;
 
 #[cxx::bridge]
 mod ffi {
-    extern "Rust" {
-        type AmbiguousRelocation;
+    enum RelocationKind {
+        ArmCall,
+        ThumbCall,
+        ArmCallThumb,
+        ThumbCallArm,
+        ArmBranch,
+        Load,
+        OverlayId,
+    }
 
+    struct AmbiguousRelocation {
+        /// The address of the relocation.
+        pub from: u32,
+        /// The address the relocation points to.
+        pub to: u32,
+        /// The source overlay ID, or `-1` if not applicable.
+        pub source_overlay: i16,
+        /// Two or more target overlay IDs that this relocation can point to.
+        pub target_overlays: Vec<u16>,
+        /// The kind of relocation.
+        pub kind: RelocationKind,
+    }
+
+    extern "Rust" {
         fn dsd_get_ambiguous_relocations(config_path: &str) -> Result<Vec<AmbiguousRelocation>>;
 
         fn dsd_melonds_init() -> u32;
     }
 }
+
+pub use ffi::*;
 
 fn dsd_melonds_init() -> u32 {
     42
--- dsd/src/relocs.rs
+++ dsd/src/relocs.rs
@@ -3,22 +3,28 @@ use std::path::{Path, PathBuf};
 use anyhow::{Context, Result};
 use ds_decomp::config::{
     config::{Config, ConfigModule},
-    relocations::{RelocationKind, RelocationModule, Relocations},
+    relocations::{RelocationModule, Relocations},
 };
 
-pub struct AmbiguousRelocation {
-    pub from: u32,
-    pub to: u32,
-    pub source_overlay: Option<u16>,
-    pub target_overlays: Vec<u16>,
-    pub kind: RelocationKind,
+use crate::{AmbiguousRelocation, RelocationKind};
+
+impl From<ds_decomp::config::relocations::RelocationKind> for RelocationKind {
+    fn from(value: ds_decomp::config::relocations::RelocationKind) -> Self {
+        match value {
+            ds_decomp::config::relocations::RelocationKind::ArmCall => RelocationKind::ArmCall,
+            ds_decomp::config::relocations::RelocationKind::ThumbCall => RelocationKind::ThumbCall,
+            ds_decomp::config::relocations::RelocationKind::ArmCallThumb => RelocationKind::ArmCallThumb,
+            ds_decomp::config::relocations::RelocationKind::ThumbCallArm => RelocationKind::ThumbCallArm,
+            ds_decomp::config::relocations::RelocationKind::ArmBranch => RelocationKind::ArmBranch,
+            ds_decomp::config::relocations::RelocationKind::Load => RelocationKind::Load,
+            ds_decomp::config::relocations::RelocationKind::OverlayId => RelocationKind::OverlayId,
+        }
+    }
 }
 
 pub fn dsd_get_ambiguous_relocations(config_path: &str) -> Result<Vec<AmbiguousRelocation>> {
     let config_path = PathBuf::from(config_path);
-    let config_dir = config_path
-        .parent()
-        .context("Config path must have a parent directory")?;
+    let config_dir = config_path.parent().context("Config path must have a parent directory")?;
     let config = Config::from_file(&config_path)?;
 
     let main = AmbiguousRelocation::get_in_module(&config.main_module, config_dir, None)?;
@@ -30,9 +36,7 @@ pub fn dsd_get_ambiguous_relocations(config_path: &str) -> Result<Vec<AmbiguousR
     let overlays = config
         .overlays
         .iter()
-        .map(|overlay| {
-            AmbiguousRelocation::get_in_module(&overlay.module, config_dir, Some(overlay.id))
-        })
+        .map(|overlay| AmbiguousRelocation::get_in_module(&overlay.module, config_dir, Some(overlay.id)))
         .collect::<Result<Vec<_>>>()?;
 
     let mut ambiguous_relocs = main;
@@ -59,9 +63,9 @@ impl AmbiguousRelocation {
                 RelocationModule::Overlays { ids } => Some(AmbiguousRelocation {
                     from: reloc.from_address(),
                     to: reloc.to_address(),
-                    source_overlay,
+                    source_overlay: source_overlay.map(|id| id as i16).unwrap_or(-1),
                     target_overlays: ids.clone(),
-                    kind: reloc.kind(),
+                    kind: reloc.kind().into(),
                 }),
                 _ => None,
             })
